535e2c8a044a127879c290d84ce554d30b1458ed
MurphyPOne <murphyp1@bishopireton.org>
2017-11-11 15:47:51 -0500

    hello

--- clock.js
+++ clock.js
@@ -1,38 +1,94 @@
 function Clock() {
-  var hr, mn, sc, time;
+  var hr, mn, sc, time, d, mnth, yr, tZone;
   angleMode(DEGREES);
 
-
   this.update = function() {
-     hr = hour();
+     hr = hour()%12;
      mn = minute();
      sc = second();
+     time = hr + ":" + mn + ":" + sc;
 
-    time = hr + ":" + mn + ":" + sc;
+     d = day();
+     mnth = month();
+     yr = year();
   }
 
   this.show = function() {
     push()
-    strokeCap(SQUARE);
-      strokeWeight(8);
-      stroke(255);
-      noFill();
-      ellipse(width - width/4, height/2, 300);
+      translate(width - width/4, height/2);
+      rotate(-90);  //puts top at 12 o clock
+      strokeCap(SQUARE);  //Sharp type *_*
+      stroke(255);  //White stroke
+      noFill(); //Removes ugly
 
-      
-      strokeWeight(4);
-      stroke(255, 100, 150);
+      //SECONDS
+      strokeWeight(6);
+      var endSC = map(sc, 0, 60, 0, 360);
+      stroke(255, 255, 255, endSC);
           //x ,           y,       ,hrad, vrad, how many
-      arc(width - width/4, height/2, 300, 300, 0, 360)
+      arc(0, 0, 280, 280, 0, endSC);
 
-    pop()
+      //MINUTES
+      strokeWeight(10);  //Size
+      var endMN = map(mn, 0, 60, 0, 360);
+      stroke(255, 255, 255, endMN);
+      arc(0, 0, 300, 300, 0, endMN);
 
+      //HOURS
+      strokeWeight(14);  //Size
+      var endHR = map(mn, 0, 24, 0, 360);
+      stroke(255, 255, 255, endHR);
+      arc(0, 0, 328, 328, 0, endHR);
+    pop();
 
     //Text formatting
     push()
-      textSize(40)
+      translate(width - width/4, height/2);
+      textSize(40);
       fill(255);
-      text(time, width - width/4, height/2)
+      text(time, -textWidth(time)/2, 0);
     pop()
   }
+  //Converts the num to a day
+  this.numToMonth = function(num) {
+    var result;
+    switch (num) {
+    case 1:
+        result = "Janury";
+        break;
+    case 2:
+        day = "February";
+        break;
+    case 3:
+        day = "March";
+        break;
+    case 4:
+        day = "April";
+        break;
+    case 5:
+        day = "May";
+        break;
+    case 6:
+        day = "June";
+        break;
+    case 7:
+        day = "July";
+        break;
+    case 8:
+        day = "August";
+        break;
+    case 9:
+        day = "September";
+        break;
+    case 10:
+        day = "October";
+        break;
+    case 11:
+        day = "November";
+        break;
+    case 12:
+        day = "December";
+        break;
+      }
+    }
 }
