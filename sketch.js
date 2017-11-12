4aabc4ccf58b90639bf732320a5a239d7a77e616
HeadJar <jarhead4123@gmail.com>
2017-11-11 21:41:49 -0500

    This is JAred 7?

--- sketch.js
+++ sketch.js
@@ -1,104 +1,133 @@
 //QuickStock$®
-var stocks = [];	//Array of stocks
-var symbols = ["aapl"] //,"googl", "aac", "mmm"];	//Stocks to fetch, TODO get from user input?
+var stocks = []; //Array of stocks
+var symbols = ["aapl", "googl"] //,"googl", "aac", "mmm"];	//Stocks to fetch, TODO get from user input?
 //Jared
 var newsOrg = ["business-insider", "fortune"];
 
-var list;
 var singleArticle;
+
+var yearWeeks = ["2017-10-20", "2017-09-22", "2017-08-11", "2017-07-14", "2017-06-23", "2017-05-12", "2017-04-21", "2017-03-17", "2017-02-03", "2017-01-20"]; //YEARS IN A WEEK
+
+
 //
 function preload() {
-	stockFont = loadFont('assets/BebasNeue.otf');
+  stockFont = loadFont('assets/BebasNeue.otf');
 }
 
 function setup() {
-	myCanvas = createCanvas(windowWidth, windowHeight);
-	//Import parrticles?
-	particlesJS.load('particles-js', 'particles.json', function() {
-  	console.log('callback - particles.js config loaded');
-	});
-
-	//Graphical Setup
-	clock = new Clock();
-	background(50); //gray bg
-	textFont(stockFont);
-
-	$(document).ready(function() {	//jQuery funciton, only called once the document is "ready" wtf that means..
-		for(j = 0; j < symbols.length; j++) {	//Iterate through the supplied symbols
-			var symbol = symbols[j];	//Grab current symbol
-			var key = 'N6N8STFNCERJ1DTH';	//Personal API Key
-			var URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol='+symbol+'&interval=1min&apikey='+key;
-
-			$.getJSON(URL, function(data) {	//Grabs the JSON from the URL, and calls a function
-				console.log(data);
-
-				if(typeof data["Meta Data"]["2. Symbol"] !== "undefined" ) {	//Verifies the fetch was successful
-					var iSymbol = data["Meta Data"]["2. Symbol"]; //Grabs official symbol from data rather than str passed by user
-					var weeks = data["Weekly Time Series"];
-					var lastWeek = data["Meta Data"]["3. Last Refreshed"];
-					var price = weeks[lastWeek]["1. open"];//Grabs open price from last week
-
-					stocks.push(new Stock(iSymbol, price) );
-				} else {
-					console.log("data missing?")
-				}
-			});
-		}
-	});
-
-	console.log(stocks);
-//Jared
-$(document).ready(function() { //jQuery funciton, only called once the document is "ready" wtf that means..
-for (j = 0; j < newsOrg.length; j++) {
-	var newsOrganization = newsOrg[j];
-
-      var URL2 = 'https://newsapi.org/v1/articles?source=' + newsOrganization + '&sortBy=top&apiKey=1e4eec7a67aa42a49ff34214aebe4f86';
-
-      $.getJSON(URL2, function(data) { //Grabs the JSON from the URL, and calls a function
-
-        var desc = "";
-        var index = i;
-        var title = "";
-        list = new LinkedList();
-
-        for (var i = 0; i < data.articles.length; i++) {
-
-          desc = data.articles[i].description;
-          index = i;
-          title = data.articles[i].title;
-          singleArticle = new Articles(index, desc, title);
-          singleArticle.print();
-
-        }
-      });
-    }
+  myCanvas = createCanvas(windowWidth, windowHeight);
+  //Import parrticles?
+  particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
   });
+
+  //Graphical Setup
+  clock = new Clock();
+  background(50); //gray bg
+  textFont(stockFont);
+
+  $(document).ready(function() { //jQuery funciton, only called once the document is "ready" wtf that means..
+    doStocks();
+    //  doArticles();
+
+  });
+
+
 }
 
-function draw() {	//TODO not drawing to the currect canvas?
-	background(50);
+function draw() { //TODO not drawing to the currect canvas?
+  background(50);
 
-	textSize(25)
-	clock.update();
-	clock.show()
+  textSize(25)
+  clock.update();
+  clock.show()
 
-	textSize(25)
-	for(var i = 0; i < stocks.length; i++) {
-		stocks[i].show(i);	//Dras the stocks to the screen
-	}
+  textSize(25)
+  for (var i = 0; i < stocks.length; i++) {
+    stocks[i].show(i); //Dras the stocks to the screen
+  }
 }
 
 function keyPressed() {
-	console.log(key);
+  console.log(key);
 }
 
 //Counts number of object in an object
-function ObjectLength( object ) {
-    var length = 0;
-    for(var key in object ) {
-        if(object.hasOwnProperty(key) ) {
-            ++length;
-        }
+function ObjectLength(object) {
+  var length = 0;
+  for (var key in object) {
+    if (object.hasOwnProperty(key)) {
+      ++length;
     }
-    return length;
+  }
+  return length;
+}
+
+
+function doStocks() {
+  for (j = 0; j < symbols.length; j++) { //Iterate through the supplied symbols
+    var symbol = symbols[j]; //Grab current symbol
+    var key = 'N6N8STFNCERJ1DTH'; //Personal API Key
+    var URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=' + symbol + '&interval=1min&apikey=' + key;
+
+    $.getJSON(URL, function(data) { //Grabs the JSON from the URL, and calls a function
+
+      if (typeof data["Meta Data"] !== "undefined") { //Verifies the fetch was successful
+        var iSymbol = data["Meta Data"]["2. Symbol"]; //Grabs official symbol from data rather than str passed by user
+        var weeks = data["Weekly Time Series"];
+        var lastWeek = data["Meta Data"]["3. Last Refreshed"];
+        var price = weeks[lastWeek]["1. open"]; //Grabs open price from last week
+        var avg = getAvg(weeks);
+
+        stocks.push(new Stock(iSymbol, price, avg));
+				stocks[0].ratingPrice();
+				stocks[1].ratingPrice();
+				console.log("total "+  stocks[0].ratingPrice() + stocks[1].ratingPrice());
+
+
+      } else {
+        console.log("data missing?")
+      }
+
+    });
+  }
+	console.log(stocks);
+
+};
+
+function doArticles() {
+  for (j = 0; j < newsOrg.length; j++) {
+    var newsOrganization = newsOrg[j];
+
+    var URL2 = 'https://newsapi.org/v1/articles?source=' + newsOrganization + '&sortBy=top&apiKey=1e4eec7a67aa42a49ff34214aebe4f86';
+
+    $.getJSON(URL2, function(data) { //Grabs the JSON from the URL, and calls a function
+
+      var desc = "";
+      var index = i;
+      var title = "";
+
+
+      for (var i = 0; i < data.articles.length; i++) {
+
+        desc = data.articles[i].description;
+        index = i;
+        title = data.articles[i].title;
+        singleArticle = new Articles(index, desc, title);
+
+        singleArticle.print();
+      }
+
+    });
+  }
+};
+
+
+function getAvg(x) {
+  var sum = 0;
+  //console.log(x);
+  for (var i = 0; i < yearWeeks.length; i++) {
+    sum += Math.round(x[yearWeeks[i]]["1. open"] * 100) / 100; //Converts from string to num
+  }
+  return sum / 12; //Returns the avg price of the last year
 }
--- stock.js
+++ stock.js
@@ -1,8 +1,8 @@
-function Stock(smbl, prc) {
+function Stock(smbl, prc, avg) {
   this.symbol = smbl.toUpperCase();
   //this.name = nm;
   this.price = Math.round(prc * 100) / 100;
-  this.avg = this.price + random(-50, 50);
+  this.avg = avg;
   this.format = " : $";
   this.displayName = this.symbol + this.format + this.price;
 
@@ -18,24 +18,24 @@ function Stock(smbl, prc) {
 
   this.show = function(offset) {
     textSize(25);
-    fill(255,255,255);	//Color to white
-    text(this.displayName, this.x, 50 + 50 * offset); 	//width and height are native vars to use too
-    fill(10,255,10);	//TODO make the color change according to $$ or alg
+    fill(255, 255, 255); //Color to white
+    text(this.displayName, this.x, 50 + 50 * offset); //width and height are native vars to use too
+    fill(10, 255, 10); //TODO make the color change according to $$ or alg
     this.currentBar(offset);
     this.avgBar(offset);
   }
 
   this.currentBar = function(offset) {
     push()
-strokeWeight(2);
-stroke(255);
-if(this.price > this.avg) { //Above Avg
-fill(50, 200, 50, 255); //transparent
-} else { //Below Avg
-fill(200, 50, 50, 255); //transparent
-}
-rect(this.width + this.padding + this.x, 30 + 50*offset, this.price/2, 20, 20, 1, 20, 1);
-pop();
+    strokeWeight(2);
+    stroke(255);
+    if (this.price > this.avg) { //Above Avg
+      fill(50, 200, 50, 255); //transparent
+    } else { //Below Avg
+      fill(200, 50, 50, 255); //transparent
+    }
+    rect(this.width + this.padding + this.x, 30 + 50 * offset, this.price / 2, 20, 20, 1, 20, 1);
+    pop();
   }
 
   this.avgBar = function(offset) {
@@ -43,7 +43,36 @@ pop();
     strokeWeight(2);
     stroke(255);
     fill(200, 200, 200, 200); //transparent
-    rect(this.width + this.padding + this.x, 30 + 50*offset, this.avg/2, 20, 20, 1, 20, 1);
+    rect(this.width + this.padding + this.x, 30 + 50 * offset, this.avg / 2, 20, 20, 1, 20, 1);
     pop();
   }
+
+  this.ratingPrice = function() {
+    var rating = 0;
+    var diff = this.price - this.avg;
+    var twentyPercent = (this.avg * .2);
+    var fifteenPercent = (this.avg * .15);
+    var fivePercent = (this.avg * .1);
+
+    if (diff > twentyPercent) {
+    rating = 5;
+  } else if (diff >= fifteenPercent && diff <= twentyPercent) {
+    rating = 4;
+  } else if (diff >= fivePercent && diff <= fifteenPercent) {
+    rating = 3;
+  } else if (diff >= 0 && diff <= fivePercent) {
+    rating = 2;
+  } else if (diff <= 0) {
+    rating = 1;
+  }
+
+
+console.log(this.symbol + "," + rating);
+console.log(this.price + " " + this.avg);
+console.log(diff + " " + twentyPercent + " " + fifteenPercent + " " + fivePercent);
+  return rating;
+
+}
+
+
 }
