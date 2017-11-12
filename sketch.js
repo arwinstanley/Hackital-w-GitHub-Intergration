e47b2c842ef4de2296335fcf8ad660aef0182ecb
MurphyPOne <murphyp1@bishopireton.org>
2017-11-11 22:11:45 -0500

    help sadnesds

--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ var symbols = []//["aapl","googl", "aac", "mmm"];
 var symbolJSON = 'stocks.json';
 var yearWeeks = ["2017-10-20", "2017-09-22", "2017-08-11", "2017-07-14", "2017-06-23", "2017-05-12", "2017-04-21", "2017-03-17", "2017-02-03", "2017-01-20"];//YEARS IN A WEEK
 
+var btcMonths = [739.00, 966.19, 1189.10, 1081.30, 1435.00, 2191.83, 2191.83, 2420.60, 2854.30, 4718.30, 4366.60, 6450.20];
+var btc;	//BTC IS TREATED LIKE A STOCK
+
 var articles = [];
 var chyX = 0;
 //Jared
@@ -31,71 +34,98 @@ function setup() {
 	$(document).ready(function() {	//jQuery funciton, only called once the document is "ready" wtf that means..
 		doStocks();
 		doArticles();
+		doBTC();
 	});
 }
 
 function draw() {	//TODO not drawing to the currect canvas?
 	background(50,50,50,0);
-
 	textSize(25)
+
+	//Clock Ish
 	clock.update();
 	clock.show()
 
+	//Stock ish
 	textSize(25)
 	for(var i = 0; i < stocks.length; i++) {
 		stocks[i].show(i);	//Dras the stocks to the screen
 	}
+	//coin
 
+	//Article ish
 	var chyron = "";
 	for(var i = 0; i < articles.length; i++ ) {
-		chyron += articles[i].title + "\t\t\t";
+		chyron += articles[i].title + "\t\t | \t\t";
 	}
-	chyX+=3;
+	if(chyX < textWidth(chyron)) {	//Repeats
+		chyX+=3;
+	} else {
+		chyX = 0;
+	}
 
+	//News ish
 	push();
 		fill(255,255,255,150);
 		text(chyron,width-chyX, height-2 );
 	pop();
 }
 
-function keyPressed() {
-	//console.log(key);
-}
-
 //Peter
 function doStocks() {
 	$.getJSON('stocks.json', function(data) {	//populates symbols from Json
-		for(var name in data) {	//Iterates through json
+		for (var name in data) {	//Iterates through stocks.json
 			symbols[name] = data[name];	//copies json to array
+			var nm = symbols[name];
+			var symbol = symbols[name];	//Grab current symbol
+			var key = 'N6N8STFNCERJ1DTH';	//Personal API Key
+			var URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol='+symbol+'&interval=1min&apikey='+key;
+
+			$.getJSON(URL, function(data) {	//Grabs the JSON from the URL, and calls a function
+				//console.log(data);
+				if(typeof data["Meta Data"] !== "undefined" ) {	//Verifies the fetch was successful
+					var iSymbol = data["Meta Data"]["2. Symbol"]; //Grabs official symbol from data rather than str passed by user
+					var weeks = data["Weekly Time Series"];
+					var lastWeek = data["Meta Data"]["3. Last Refreshed"];
+					var price = weeks[lastWeek]["1. open"];//Grabs open price from last week
+					var avg = getAvg(weeks);
+					stocks.push(new Stock(iSymbol, name, price, avg) );
+				} else {
+					console.log("data missing?")
+				}
+			});
 		}
 	});
+}
 
-	for(var s in symbols) {	//Iterate through the supplied symbols
-		var symbol = s	//Grab current symbol
-		var key = 'N6N8STFNCERJ1DTH';	//Personal API Key
-		var URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol='+symbol+'&interval=1min&apikey='+key;
+//Creates a bitcoin ticker
+function doBTC() {
+		var URL = 'https://blockchain.info/ticker';
+		$.getJSON(URL, function(data) {
+			console.log("poop");
+			var price = data.USD.buy;
+			var symbol = "BTC";
+			var name = "Bitcoin";
+			var avg = getBTCAvg();
+			btc = new Stock(symbol, name, price, avg);
+			stocks.push(btc);
 
-		$.getJSON(URL, function(data) {	//Grabs the JSON from the URL, and calls a function
-			//console.log(data);
+	});
+}
 
-			if(typeof data["Meta Data"] !== "undefined" ) {	//Verifies the fetch was successful
-				var iSymbol = data["Meta Data"]["2. Symbol"]; //Grabs official symbol from data rather than str passed by user
-				var weeks = data["Weekly Time Series"];
-				var lastWeek = data["Meta Data"]["3. Last Refreshed"];
-				var price = weeks[lastWeek]["1. open"];//Grabs open price from last week
-				var avg = getAvg(weeks);
-				stocks.push(new Stock(iSymbol, price, avg) );
-			} else {
-				console.log("data missing?")
-			}
-		});
+function getBTCAvg() {
+	var sum = 0;
+
+	for(var i = 0; i < btcMonths.length; i++) {
+		sum += Math.round(btcMonths[i] * 100) /100;
 	}
+	return sum/12;
 }
 
 //Peter
 function getAvg(x) {
 	var sum = 0;
-	console.log(x);
+	//console.log(x);
 	for(var i = 0; i < yearWeeks.length; i++) {
 		sum += Math.round(x[yearWeeks[i]]["1. open"] * 100) / 100;	//Converts from string to num
 	}
